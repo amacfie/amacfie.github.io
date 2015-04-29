6ae705b9f5ec46d296a10d46324cbfca179819be
Andrew MacFie <amacfie@sent.com>
2015-04-28 21:37:58 -0400

    Add shell script for creating posts

---
+++ createPost.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "Pass the post name filename suffix"
+  exit 1
+fi
+fn="`dirname $0`/_posts/`date +\%Y-\%m-\%d-`$1.md"
+
+touch $fn
+echo "---" > $fn
+echo "layout: post" >> $fn
+echo "title: <ENTER TITLE HERE>" >> $fn
+echo "---" >> $fn
+printf "\n" >> $fn
+vi $fn -c "normal zR3ggf<vf>"
+
